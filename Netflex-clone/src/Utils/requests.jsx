06030bb1ddc58dcc28cd4ed613de1ed2731c3cc8
endalamawy <endalamawteka23@gmail.com>
2025-09-07 14:30:56 -0500

    banner

--- Netflex-clone/src/Utils/requests.jsx
+++ Netflex-clone/src/Utils/requests.jsx
@@ -1,5 +1,5 @@
 // const VITE_KEY = process.env.VITE_API_API_KEY;
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
