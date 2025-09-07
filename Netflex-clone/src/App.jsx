06dd0e5c40a13747518bb8b2da1ea598ac402c20
endalamawy <endalamawteka23@gmail.com>
2025-09-07 10:27:07 -0500

    header and footer

--- Netflex-clone/src/App.jsx
+++ Netflex-clone/src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home/Home'
+import './App.css';
+import Home from './Pages/Home/Home';
+
 
 function App() {
-const [count, setCount] = useState(0)
-
-return (
-<>
-   
-      <Home/>
-</>
-)
+  return (
+    <div className="App">
+     <Home/>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
 
 
 // import { useState } from 'react'
